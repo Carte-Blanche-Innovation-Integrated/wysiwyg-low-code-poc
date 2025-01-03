8c8f0da196e75c87b1ab33fa0293408e1fc0bb95
Abdullah <abdullah.ajmal.7@hotmail.com>
2025-01-03 22:48:09 +0500

    Add Heroku-hosted PG database URL to the page
    Included a new list item displaying the PostgreSQL database URL from environment variables. This enhances clarity for users by explicitly showing the database location.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>You can access PG(hosted on Heroku) on: {process.env.DATABASE_URL}</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
