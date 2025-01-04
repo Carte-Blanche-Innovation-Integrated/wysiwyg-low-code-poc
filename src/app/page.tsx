bd1f121e051e1aded102f39cc18cd3544f35fb46
Nauman Umer <nmanumr@gmail.com>
2025-01-04 22:48:16 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,13 @@
-import {SidebarTrigger} from "@/components/ui/sidebar";
-import {Separator} from "@/components/ui/separator";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
-} from "@/components/ui/breadcrumb";
+'use client';
+
+import dynamic from "next/dynamic";
+
+const NocoApp = dynamic(() => import('./[...slug]/dynamic-app'), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
-    <>
-      <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-        <div className="flex items-center gap-2 px-3">
-          <SidebarTrigger/>
-          <Separator orientation="vertical" className="mr-2 h-4"/>
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block"/>
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-      </header>
-    </>
+    <NocoApp/>
   );
 }
